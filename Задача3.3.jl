0a02b8414434872bcea74d251ffb6836d05c411a
Лева <121401683+LevGurev@users.noreply.github.com>
2023-10-12 13:13:45 +0300

    Update Задача3.3.jl

--- Задача3.3.jl
+++ Задача3.3.jl
@@ -1,20 +1,22 @@
-﻿function factorize(n::IntType) where IntType <: Integer
+# Функция degree(n, p) вычисляет кратность делителя `p` в числе `n`.
+function degree(n, p)
+    k = 0
+    n, r = divrem(n, p)
+    while n > 0 && r == 0
+        k += 1
+        n, r = divrem(n, p)
+    end
+    return k
+end
+
+# Функция factorize(n::IntType) выполняет факторизацию числа `n` на простые множители.
+function factorize(n::IntType) where IntType <: Integer
     list = NamedTuple{(:div, :deg), Tuple{IntType, IntType}}[]
-    for p in eratosphenes_sieve(Int(ceil(n/2)))
-        k = degree(n, p) # кратность делителя
+    for p in eratosphenes_sieve(Int(ceil(n / 2)))
+        k = degree(n, p) # Кратность делителя
         if k > 0
             push!(list, (div=p, deg=k))
         end
     end
     return list
-end
-
-function degree(n, p) # кратность делителя `p` числа `n`
-    k=0
-    n, r = divrem(n,p)
-    while n > 0 && r == 0
-        k += 1
-        n, r = divrem(n,p)
-    end
-    return k
 end
